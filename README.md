19f84125bf345a95f88d0f84ab10f9dd9c17a90f
Gregory Mckenzie <gregoryjmckenzie7@gmail.com>
2018-03-06 17:06:51 -0500

    working application, added to readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Bamazon
 App similar to amazon using node
+
+In order to use this app you will need to install node.js, npm, mysql/mysql npm, console.table, and inquirer.
+Clone the repository found on this page https://github.com/gregoryjmckenzie7/Bamazon.
+In the terminal of your choice, navigate to the folder that you cloned the repository into.
+
+Execute the following command lines:
+npm install console.table
+npm install inquirer
+npm install my-sql
+
+In a SQL database system, create a database with a table and products like the one found in the bamazon.sql file.
+
+At that point you will be able to run the application in your terminal by typing the following command:
+
+node bamazon.js
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -14,11 +15,83 @@ connection.connect(function(err) {
     start();
 });
 
-funtion start() {
-    connection.query('SELECT * FROM `products`', function(err, res){
+function start(){
+    connection.query("SELECT * FROM `products`", function(err, res){
         if (err) { throw err; }
         if (res && res.length){
             console.table(res);
         }
+        inquirer
+        .prompt([
+        {    
+            name: "choice",
+            message: "What is the item number of the product you would like to buy? or type 'q' to quit",
+            validate: function(value) {
+                if(value === 'q' || isNaN(value) === false && parseInt(value) > 0) {
+                    return true;
+                }
+                return false;
+            }
+        }]).then(function (answer) {
+            if(answer.choice === 'q') {
+                connection.end();
+            }
+            else {
+                quantity(answer.choice);
+            }
+        });
     });
+}
+function quantity(choice) {
+    var itemId = choice;
+    inquirer
+    .prompt([
+        {
+        name: "quantity",
+        message: "How many of them would you like to buy? or type 'q' to quit",
+        validate: function(value) {
+            if (value === 'q' || isNaN(value) === false && parseInt(value) > 0) {
+                return true;
+            }
+            return false;
+        }
+        }]).then(function(answer) {
+            if (answer.quantity === 'q') {
+                connection.end();
+            }
+            else {
+                stockCheck(itemId, answer.quantity);
+            }
+        });
+}
+function stockCheck(id, quantity) {
+    var amount = parseInt(quantity);
+    connection.query("SELECT stock_quantity, price FROM `products` WHERE ?", {item_id: id}, function (err, res){
+        var price = Number(res[0].price);
+        if (err) throw err;
+        if (res && res.length) {
+            if (res[0].stock_quantity >= amount) {
+                connetion.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: (res[0].stock_quantity - amount)
+                        },
+                        {
+                            item_id: id
+                        },
+                    ],
+                    function (err) {
+                        if (err) throw err;
+                        console.log("Congratlations on your purchase, your total is $ " + (price*amount));
+                        start();
+                    }
+                );
+            }
+            else {
+                console.log("Unfortunately we do not have the availble stock to fulfill that order");
+                start();
+            }
+        }
+ });
 }
